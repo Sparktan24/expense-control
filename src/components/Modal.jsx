2559d84605e79ffb1ac2f331ab65204cb60d625d
Jesus Molina <netod90@gmail.com>
2023-04-13 16:25:57 -0700

    Add form to Modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -16,6 +16,39 @@ const Modal = ({ setModal, animateModal, setAnimateModal }) => {
 
       <form className={`formulario ${animateModal ? "animar" : "cerrar"}`}>
         <legend>New Expense</legend>
+
+        <div className="campo">
+          <label htmlFor="name">Expense Name</label>
+          <input
+            id="name"
+            type="text"
+            placeholder="Add the name of the expense"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="amount">Amount</label>
+          <input
+            id="amount"
+            type="number"
+            placeholder="Add the expense amount"
+          />
+        </div>
+
+        <div className="campo">
+          <label htmlFor="category">Category</label>
+          <select id="category">
+            <option value="">--Select--</option>
+            <option value="savings">Savings</option>
+            <option value="food">Food</option>
+            <option value="house">House</option>
+            <option value="misc">Misc</option>
+            <option value="fun">Fun</option>
+            <option value="health">Health</option>
+            <option value="suscriptions">Suscriptions</option>
+          </select>
+        </div>
+        <input type="submit" value="Add expense" />
       </form>
     </div>
   );
