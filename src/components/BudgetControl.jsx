5d259869bc5537a126a6bf101b123752a11b95e6
Jesus Molina <netod90@gmail.com>
2023-04-13 12:16:26 -0700

    Add BudgetControl structure & money format

--- src/components/BudgetControl.jsx
+++ src/components/BudgetControl.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 const BudgetControl = ({ budget }) => {
+  const formatAmount = (amount) => {
+    return amount.toLocaleString("en-US", {
+      style: "currency",
+      currency: "USD",
+    });
+  };
+
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
@@ -8,7 +15,17 @@ const BudgetControl = ({ budget }) => {
       </div>
 
       <div className="contenido-presupuesto">
-        <span>Budget: </span> ${budget}
+        <p>
+          <span>Budget: </span> {formatAmount(budget)}
+        </p>
+
+        <p>
+          <span>Available: </span> {formatAmount(0)}
+        </p>
+
+        <p>
+          <span>Spent: </span> {formatAmount(0)}
+        </p>
       </div>
     </div>
   );
