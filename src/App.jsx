d8a0513c7132c7cf2b38a1236c53d9730e09ee30
Jesus Molina <netod90@gmail.com>
2023-04-18 08:11:50 -0700

    Add icons to expenses

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
   };
 
   return (
-    <div>
+    <div className={modal && "fijar"}>
       <Header
         budget={budget}
         setBudget={setBudget}
--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,11 +1,30 @@
 import React from "react";
 import { formatDate } from "../helpers";
 
+import IconoAhorro from "../img/icono_ahorro.svg";
+import IconoCasa from "../img/icono_casa.svg";
+import IconoComida from "../img/icono_comida.svg";
+import IconoGastos from "../img/icono_gastos.svg";
+import IconoOcio from "../img/icono_ocio.svg";
+import IconoSalud from "../img/icono_salud.svg";
+import IconoSuscripciones from "../img/icono_suscripciones.svg";
+
+const iconDictionary = {
+  savings: IconoAhorro,
+  food: IconoComida,
+  house: IconoCasa,
+  misc: IconoGastos,
+  fun: IconoOcio,
+  health: IconoSalud,
+  suscriptions: IconoSuscripciones,
+};
+
 const Expense = ({ expense }) => {
   const { expenseName, expenseAmount, expenseCategory, id, date } = expense;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
+        <img src={iconDictionary[expenseCategory]} alt="Expense icon" />
         <div className="descripcion-gasto">
           <p className="categoria">{expenseCategory}</p>
           <p className="nombre-gasto">{expenseName}</p>
