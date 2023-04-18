4f45245e7574e76be6a6e176aca6ae573683f8e6
Jesus Molina <netod90@gmail.com>
2023-04-18 07:51:24 -0700

    Add Date format to Expense

--- src/components/Expense.jsx
+++ src/components/Expense.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { formatDate } from "../helpers";
 
 const Expense = ({ expense }) => {
-  const { expenseName, expenseAmount, expenseCategory, id } = expense;
+  const { expenseName, expenseAmount, expenseCategory, id, date } = expense;
   return (
     <div className="gasto sombra">
       <div className="contenido-gasto">
         <div className="descripcion-gasto">
           <p className="categoria">{expenseCategory}</p>
           <p className="nombre-gasto">{expenseName}</p>
+          <p className="fecha-gasto">
+            Added on: {""}
+            <span>{formatDate(date)}</span>
+          </p>
         </div>
       </div>
+      <p className="cantidad-gasto">${expenseAmount}</p>
     </div>
   );
 };
